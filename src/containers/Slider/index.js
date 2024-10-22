5d9f08b2d2e74751bf87885cd2f0a000ef85cc53
oanaciuca <oana.daniela.ciuca@gmail>
2024-10-23 00:01:20 +0200

    Correction de la logique de mise à jour de l'index dans le carrousel

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -11,11 +11,11 @@ const Slider = () => {
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setTimeout(() => {
+      setIndex((prevIndex) => prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0);
+    }, 5000);
   };
+  
   useEffect(() => {
     nextCard();
   });
